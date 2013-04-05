825250c0087a4f064e77e87e6d351e7c1f82a201
pszjp <james.pinchin@nottingham.ac.uk>
2013-04-06 00:07:13 +0100

    Modified to allow batch processing of maze trial data

--- PParticle.m
+++ PParticle.m
@@ -15,6 +15,7 @@ baseDir = pwd;
 
 %% Open control file
 
+LoadINI
 
 ctrlFile = importdata('G:\Projects\OI Trial\ctrlFile.txt');
 nextLine = min(find(ctrlFile(:,4)==0));
@@ -26,16 +27,34 @@ end
 
 a = [['cd '] [''''] ['G:\data\OITrial_Jan2013\Participants'] ['\'] [participant] ['\'] [trialNo] ['\'] ['''']];
 
-
+if ctrlFile(nextLine,3)==1
+    ini.north = 28.7;
+    ini.east = 39.7;
+    ini.Heading = 0;
+elseif ctrlFile(nextLine,3)==2
+    ini.north = 43.4;
+    ini.east = 39.7;
+    ini.Heading = 0;
+elseif ctrlFile(nextLine,3)==3
+    ini.north = 43.4;
+    ini.east = 54.5;
+    ini.Heading = 0;
+elseif ctrlFile(nextLine,3)==4
+    ini.north = 28.7;
+    ini.east = 52.7;
+    ini.Heading = 0;
+end
 %% ini values
 
 % a = [['cd '] [''''] [pwd] ['\'] ['SampleData'] ['''']];
 % a = [['cd '] [''''] [pwd] ['\'] ['SampleData\Maze'] ['''']];
 eval(a);
-% cd 'G:\data\EdinDump\Walk2-HolyroodtoGeog\Proc'
-LoadINI
-% load 'BinaryMap.mat'
-%cd 'C:\Users\pszjp\Documents\MATLAB\ParticleFilters\PParticle'
+
+% LoadINI
+
+ini.fins = [[pwd] '\ins.txt'];
+ini.HistoryOP = [[pwd] '\ParticleHistory.txt'];
+
 a = [['cd '] [''''] [baseDir] ['\'] ['''']];
 eval(a);
 
@@ -114,8 +133,6 @@ end
 
 [posn] = getHistory(ini,observation);
 
-fclose all
-
 ctrlFile(nextLine,4) = 1;
 if FilterFail == true
     ctrlFile(nextLine,5) = 1;
@@ -126,7 +143,7 @@ for i = 1:size(ctrlFile,1)
     fprintf(fid,'%i,%i,%i,%i,%i \n',ctrlFile(i,:)); 
 end
 
-
+fclose all
 
 
 
